5e316b40e0a210212cd4ad2f0028ab6ffe644a9c
teg0023 <teg0023@auburn.edu>
2024-02-05 13:54:05 -0600

    Initial Commit

---
+++ MyAdventure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAdventure
+//
+//  Created by user248010 on 2/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
