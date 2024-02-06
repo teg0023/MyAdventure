d01b85aabbb28ca9a3fdd36e34d9677693a489b1
teg0023 <teg0023@auburn.edu>
2024-02-06 16:34:17 -0600

    Created a struct.

--- MyAdventure/View/ViewController.swift
+++ MyAdventure/View/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var EndGameLabel: UILabel!
+    @IBOutlet weak var ScenarioLabel: UILabel!
+    
+    
+    @IBOutlet weak var Choice2BTN: UIButton!
+    @IBOutlet weak var Choice1BTN: UIButton!
+    
+    var roadProblem = RoadChoiceLogic()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ScenarioLabel.text = roadProblem.getScenario()
+        EndGameLabel.text = ""
+        Choice1BTN.setTitle("Road \(roadProblem.getChoice1())", for: .normal)
+        Choice2BTN.setTitle("Road \(roadProblem.getChoice2())", for: .normal)
     }
 
 
+    @IBAction func userAnswer(_ sender: UIButton) {
+        if (sender.title(for: .normal) == "Road \(roadProblem.getChoice1())")
+        {
+            EndGameLabel.text = roadProblem.setIndex(indexIn: roadProblem.getChoice1())
+            ScenarioLabel.text = roadProblem.getScenario()
+            Choice1BTN.setTitle("Road \(roadProblem.getChoice1())", for: .normal)
+            Choice2BTN.setTitle("Road \(roadProblem.getChoice2())", for: .normal)
+        }
+        else
+        {
+            EndGameLabel.text = roadProblem.setIndex(indexIn: roadProblem.getChoice2())
+            ScenarioLabel.text = roadProblem.getScenario()
+            Choice1BTN.setTitle("Road \(roadProblem.getChoice1())", for: .normal)
+            Choice2BTN.setTitle("Road \(roadProblem.getChoice2())", for: .normal)
+        }
+        
+    }
 }
 
